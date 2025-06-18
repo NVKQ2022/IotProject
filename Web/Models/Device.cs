e4b25aeb14aa8af25876d0ad8b21f69b109428cb
nguyen quan <nguyenvietkyquan2022@gmail.com>
2025-06-19 01:51:48 +0700

    Push full ASP.NET solution into Web folder

---
+++ Web/Models/Device.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Web_for_IotProject.Models
+{
+    public class Device
+    {
+        public string DeviceId { get; set; }
+        public string DeviceName { get; set; }
+        public string Location { get; set; }
+        public string IpAddress { get; set; }
+        public string MacAddress { get; set; }
+        public bool Status { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public int Quality { get; set; }
+
+        public int FPS { get; set; }
+        public string LastSeen { get; set; }
+    }
+
+    
+}
---
+++ Web/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_for_IotProject.Models
+{
+    public class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public User()
+        {
+
+        }
+
+    }
+}
