5b9e56761a3194875400bdce8b3e08d3346db53f
NVKQ2022 <143944625+NVKQ2022@users.noreply.github.com>
2025-06-19 01:54:50 +0700

    Delete Web directory

--- Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_for_IotProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
